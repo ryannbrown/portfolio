6d8eafc923770c0808b3cc83382c0bf623606067
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-05-29 12:06:30 -0400

    making updates

---
+++ precache-manifest.f0ff13ddc298654c6a80ff01dd844e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f04f4e5af665f5eb67d82522c658886",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "85036d10b19aa7eb1094",
+    "url": "/portfolio/static/css/main.1e9b2433.chunk.css"
+  },
+  {
+    "revision": "bd0a079399f214f76d4e",
+    "url": "/portfolio/static/js/2.f57d7428.chunk.js"
+  },
+  {
+    "revision": "85036d10b19aa7eb1094",
+    "url": "/portfolio/static/js/main.0b7d29d7.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "97b7d7c7a97345ac2b708f56cfb191af",
+    "url": "/portfolio/static/media/Ryan.97b7d7c7.jpg"
+  }
+]);
